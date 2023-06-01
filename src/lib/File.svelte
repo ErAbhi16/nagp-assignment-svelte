b8d2ebef0ffc62c2248d47894ac12490bfaadb0b
Abhinav Goyal <abhinav.goyal@onsolve.com>
2023-06-01 15:41:50 +0530

    indentation fix

--- src/lib/File.svelte
+++ src/lib/File.svelte
@@ -2,13 +2,6 @@
 <script>
 	export let name;
 </script>
-<span style="font-size: 0.9rem;
-padding: 0.5rem 0.063rem 0.5rem 0.063rem;
-color: #3777bc;
-border-color: #fff;
-background-color: #fff;
-font-weight: bold;
-text-align: left;
-color: #3777bc;">{name}</span>
+<span style="font-size: 0.9rem;padding: 0.5rem 0.063rem 0.5rem 0.063rem;color: #3777bc;border-color: #fff;background-color: #fff;font-weight: bold;text-align: left;color: #3777bc;">{name}</span>
 
 
--- src/lib/Folder.svelte
+++ src/lib/Folder.svelte
@@ -1,25 +1,16 @@
 <svelte:options tag="folder-app" />
 <script>
 	import File from './File.svelte';
-
 	export let resources = [];
 </script>
 
 {#each resources as resource}
-   <ul style=" padding: 0.5rem 0.938rem 0.5rem 0.938rem;
-   margin: 0 0 0 0.313rem;
-   list-style: none;
-   border-left: 0.05rem solid #3777BC;">
+   <ul style=" padding: 0.5rem 0.938rem 0.5rem 0.938rem; margin: 0 0 0 0.313rem; list-style: none; border-left: 0.05rem solid #3777BC; border-bottom: 0.05rem solid #3777BC;">
     
-        {#if resource.category == "Folder"}
-        
-            <button style=" border: none;color: #fff; border-color: #3777bc; background-color: #3777bc;
-                font-weight: 700; box-shadow: 0 0.063rem 0.188rem 0 rgba(0,0,0,.2);
-                width: 4rem; height: 2rem;">{resource.name}</button>
+        {#if resource.category == "Folder"}        
+            <button style=" border: none;color: #fff; border-color: #3777bc; background-color: #3777bc; font-weight: 700; box-shadow: 0 0.063rem 0.188rem 0 rgba(0,0,0,.2); width: 4rem; height: 2rem;">{resource.name}</button>
         {/if}
-
-        {#if resource.expanded}
-        
+        {#if resource.expanded}        
             {#if resource.resources.length > 0}
                 <li style=" padding: 0.5rem 0.938rem 0.5rem 0.938rem">
                     <svelte:self {...resource} />
